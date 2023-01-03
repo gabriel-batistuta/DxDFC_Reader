4c62e7d74494279bebef62238c2e36de8a8405ec
gabriel <batistutag190@gmail.com>
2023-01-03 16:29:41 -0300

    update: img.py (funcionalidade de baixar a imagem criada)

--- modules/img.py
+++ modules/img.py
@@ -1,7 +1,21 @@
-def baixaImg(_site):
+import urllib.request
+import sys
+import os
+
+def pegaImg(_site):
     _img = _site.find('img')
     if _img['border'] == '0':
         _imagem = _img['src']
         return _imagem
     else:
         print('\n\nNão foi possível achar uma imagem!\n\n')
+
+def downloadImage(_img, _title):
+    try: 
+        urllib.request.urlretrieve(_img, f'novel/{_title}.jpg')
+        currentDirectory = os. getcwd()
+        _routeImg = f'{currentDirectory}/novel/{_title}.jpg'
+        return _routeImg
+    except:
+        erro = sys.exc_info()
+        print("Ocorreu um erro com o download da imagem: ", erro)
