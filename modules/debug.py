e2d3007173df98ddd024dd7c94a1f56faa4c44d0
gabriel <batistutag190@gmail.com>
2023-01-03 21:31:53 -0300

    add: debug.py arquivo de função de debug de script criado

---
+++ modules/debug.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+# if os.path.isdir('novel'):
+#     print('"/novel" existe!')
+# else:
+#     print('"/novel" não existe!')
+
+# if os.path.isdir('novels'):
+#     print('tudo ok')
+#     pass
+# else:
+#     os.mkdir('./novels')
+#     print('/novels criada')
+
+# if os.path.isdir('./novels/dxd'):
+#     print('diretório dxd existe!')
+# else:
+#     print('diretório dxd não existe')
+#     os.mkdir('./novels/dxd')
+
+def cleanDiretory():
+    dirPath = './novels'
+    if os.path.isdir(dirPath):
+        if len(os.listdir(dirPath)) != 0:
+            # print('tem arquivos no diretório')
+            try:
+                shutil.rmtree(dirPath)
+            except OSError as e:
+                print(f"Error:{ e.strerror}")
+        else:
+            # print('diretório vazio')
+            os.rmdir('./novels')
+    else:
+        pass
