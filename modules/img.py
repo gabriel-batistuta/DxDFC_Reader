06b9ac7deb00e5adc7e27b0212416025b8ac3eb0
gabriel <batistutag190@gmail.com>
2023-01-05 14:01:03 -0300

    update: img.py, change(adicionada cover a nome de imagem para identificar que é a capa da novel)

--- modules/img.py
+++ modules/img.py
@@ -2,7 +2,7 @@ import urllib.request
 import sys
 import os
 
-def pegaImg(_site):
+def getImg(_site):
     _img = _site.find('img')
     if _img['border'] == '0':
         _imagem = _img['src']
@@ -12,9 +12,9 @@ def pegaImg(_site):
 
 def downloadImage(_img, _title):
     try: 
-        urllib.request.urlretrieve(_img, f'novels/{_title}/{_title}.jpg')
+        urllib.request.urlretrieve(_img, f'novels/{_title}/cover - {_title}.jpg')
         currentDirectory = os. getcwd()
-        _routeImg = f'{currentDirectory}/novels/{_title}/{_title}.jpg'
+        _routeImg = f'{currentDirectory}/novels/{_title}/cover - {_title}.jpg'
         return _routeImg
     except:
         erro = sys.exc_info()
