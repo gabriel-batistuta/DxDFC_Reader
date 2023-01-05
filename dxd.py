64cd5ea1576f33416cb18aa07dc4f1de7de62385
gabriel <batistutag190@gmail.com>
2023-01-05 12:44:05 -0300

    update dxd.py, add (formatação de cores a print de caminho de arquivos)

--- dxd.py
+++ dxd.py
@@ -1,4 +1,6 @@
 from modules import url, site, img, title, link, novel, debug
+import os
+
 
 def callModules():
     # debug.cleanDiretory()
@@ -15,4 +17,14 @@ url, site, img, title, links, novel, routeImg = callModules()
 
 # site = BeautifulSoup(requests.get(input('Digite a URL com o volume a novel DxD: ')).content, 'html.parser')
 
-print(f'\n\nSITE: {url}\n\nNOVEL: {title}\n\nLINK DA IMAGEM: {img}\n\nIMAGEM em: {routeImg}\n\nNOVEL em: {novel}')
+RED   = "\033[1;31m"  
+BLUE  = "\033[1;34m"
+CYAN  = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD    = "\033[;1m"
+REVERSE = "\033[;7m"
+
+os.system('clear')
+
+print(f'\n\n{RED}SITE:{RESET} {url}\n\n{CYAN}NOVEL:{RESET} {title}\n\n{RED}LINK DA IMAGEM:{RESET} {img}\n\n{CYAN}IMAGEM em:{RESET} {routeImg}\n\n{RED}NOVEL em:{RESET} {novel}')
