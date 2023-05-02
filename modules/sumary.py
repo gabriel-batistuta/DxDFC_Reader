34bfae3a7c40496fe617066801fb6a5fda8358dd
gabriel <batistutag190@gmail.com>
2023-05-01 21:27:00 -0300

    update sumary, file updated and refatored

--- modules/sumary.py
+++ modules/sumary.py
@@ -1,19 +1,6 @@
-import os
-
 def writeSumary(links, title):
-    
-    def writeInSumary(links, title):
-        with open(f'novels/{title}/sumary - {title}.txt','a+') as file:
-            for link in links:
-                file.write(f'{link.text}\n\n')
-            file.close()
 
-    def log():
-        currentDirectory = os. getcwd()
-        sumary = f'{currentDirectory}/novels/{title}/sumary - {title}.txt'
-
-        return sumary
-    
-    writeInSumary(links, title)
-
-    return log()
+    with open(f'novels/{title}/sumary - {title}.txt','a+') as file:
+        for link in links:
+            file.write(f'{link.text}\n\n')
+        file.close()
