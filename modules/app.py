28264e9a70c98173c17fb32cc6199a65f8c663fe
gabriel-batistuta <batistutag190@gmail.com>
2024-01-15 11:39:36 -0300

    feat: opção de pdf colocada acima das demais

--- modules/app.py
+++ modules/app.py
@@ -4,16 +4,16 @@ def menu():
     options = [
         inquirer.List('option',
         message='Qual o formato de arquivo deseja?',
-        choices=['.txt', '.pdf', 'Sair'])
+        choices=['.pdf', '.txt', 'Sair'])
     ]
 
     response = inquirer.prompt(options)
 
-    if response['option'] == '.txt':
-        resp = 'txt'
-        
     elif response['option'] == '.pdf':
         resp = 'pdf'
+
+    if response['option'] == '.txt':
+        resp = 'txt'
         
     elif response['option'] == 'Sair':
         quit()
